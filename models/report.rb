76c55b0304e310b768ba9fa54bcc75608230daa8
Anders Windelhed <anders.windelhed@gmail.com>
2013-09-23 12:29:37 +0200

    fixes...

--- models/report.rb
+++ models/report.rb
@@ -1,5 +1,14 @@
 class Report
-  attr_accessor :repos
-  attr_accessor :test
+	attr_accessor :repos
+	attr_accessor :old_feature_brances
+	attr_accessor :unmerged_releasebrances
+	attr_accessor :master_branch_with_commits_missing_from_develop
+
+	def initialize
+		@repos = Array.new
+		@old_feature_brances = Array.new
+		@unmerged_releasebrances = Array.new
+		@master_branch_with_commits_missing_from_develop = Array.new
+	end
 
 end
