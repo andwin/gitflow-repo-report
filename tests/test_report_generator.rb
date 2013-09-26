2e98dcd06500b2620f244a6ae89b90ee15a00be5
Emil Wall <emil.wall@valtech.se>
2013-09-26 18:23:19 +0200

    Add get_branch_names method to report_generator
    This returns all branches of all repos in the repository path,
    passed as argument to the initialize method of ReportGenerator.

--- tests/test_report_generator.rb
+++ tests/test_report_generator.rb
@@ -19,4 +19,11 @@ class TestReportGenerator < Test::Unit::TestCase
 		assert_equal(2, repos.count)
 	end
 
+	def test_get_branch_names
+		branch_names = @report_generator.get_branch_names
+		assert_equal('test-repo-1 develop', branch_names[0])
+		assert_equal('test-repo-2 release/second-release', branch_names[6])
+		assert_equal(7, branch_names.count)
+	end
+
 end
