5b1b750dfc56d8c8c2f533748a958e659c5e0e2b
Emil Wall <emil.wall@valtech.se>
2013-09-27 08:33:10 +0200

    Use include matcher
    Since position in the array is not important at this stage.

--- tests/report_generator_spec.rb
+++ tests/report_generator_spec.rb
@@ -19,8 +19,8 @@ describe ReportGenerator do
 		end
 
 		it "should return test-repo 1 and 2" do
-			expect(@repos[0]).to eq('test-repo-1') # Should use another matcher, such as 'to contain' (same with branch_names below)
-			expect(@repos[1]).to eq('test-repo-2') # Does the order matter?
+			expect(@repos).to include('test-repo-1')
+			expect(@repos).to include('test-repo-2')
 		end
 
 		it "should return exactly 2 repos" do
@@ -38,11 +38,11 @@ describe ReportGenerator do
 		end
 
 		it "should return develop branch of test-repo-1" do
-			expect(@branch_names[0]).to eq('test-repo-1 develop')
+			expect(@branch_names).to include('test-repo-1 develop')
 		end
 
 		it "should return second release branch of test-repo-2" do
-			expect(@branch_names[6]).to eq('test-repo-2 release/second-release')
+			expect(@branch_names).to include('test-repo-2 release/second-release')
 		end
 	end
 end
