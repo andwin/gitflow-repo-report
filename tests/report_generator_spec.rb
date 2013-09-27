c063a0cfb8c9ee6ef875d55578b06b33998e1dbd
Emil Wall <emil.wall@valtech.se>
2013-09-27 08:35:35 +0200

    Improve semantics in context and spec descriptions

--- tests/report_generator_spec.rb
+++ tests/report_generator_spec.rb
@@ -13,35 +13,35 @@ describe ReportGenerator do
 		@file_util.remove_tmp_repo_dir
 	end
 
-	context "when calling get_repos" do
+	context "result of call to get_repos" do
 		before(:each) do
 			@repos = @report_generator.get_repos
 		end
 
-		it "should return test-repo 1 and 2" do
+		it "should include test-repo 1 and 2" do
 			expect(@repos).to include('test-repo-1')
 			expect(@repos).to include('test-repo-2')
 		end
 
-		it "should return exactly 2 repos" do
+		it "should contain exactly 2 repos" do
 			expect(@repos.count).to eq(2)
 		end
 	end
 
-	context "when calling get_branch_names" do
+	context "result of call to get_branch_names" do
 		before(:each) do
 			@branch_names = @report_generator.get_branch_names
 		end
 
-		it "should return correct number of branch names (all of them)" do
+		it "should contain correct number of branch names (all of them)" do
 			expect(@branch_names.count).to eq(7)
 		end
 
-		it "should return develop branch of test-repo-1" do
+		it "should include develop branch of test-repo-1" do
 			expect(@branch_names).to include('test-repo-1 develop')
 		end
 
-		it "should return second release branch of test-repo-2" do
+		it "should include second release branch of test-repo-2" do
 			expect(@branch_names).to include('test-repo-2 release/second-release')
 		end
 	end
