6c7341dada39b32172d96c92bd0dc33eeb6772df
andwin <anders.windelhed@gmail.com>
2013-10-16 21:22:17 +0200

    Added rack-test

---
+++ tests/gitflow_repo_report_spec.rb
@@ -0,0 +1,19 @@
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../gitflow_repo_report.rb'
+require 'rspec'
+require 'rack/test'
+
+describe 'The gitflow repo report App' do
+	include Rack::Test::Methods
+
+	def app
+		Sinatra::Application
+	end
+
+	it "loads ok" do
+		get '/'
+		expect(last_response).to be_ok
+		expect(last_response.body).to include('GitFlow report')
+	end
+end
