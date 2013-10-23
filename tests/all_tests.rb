d2eac6a2bde8ca0f1dc308519e582e1c3a86cb33
andwin <anders.windelhed@gmail.com>
2013-10-23 19:46:54 +0200

    Cleanup

--- tests/all_tests.rb
+++ tests/all_tests.rb
@@ -1,6 +1,5 @@
 Dir.chdir(File.dirname(__FILE__)) do
-  Dir.glob('**/test_*.rb') do |test_case| 
-  	puts File.expand_path(File.dirname(File.dirname(__FILE__)))
-    require "#{File.expand_path(File.dirname(File.dirname(__FILE__)))}/#{test_case}" 
-  end
+	Dir.glob('**/test_*.rb') do |test_case| 
+		require "#{File.expand_path(File.dirname(File.dirname(__FILE__)))}/#{test_case}" 
+	end
 end
