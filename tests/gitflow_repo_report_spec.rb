7fe396f29075a0144eb1a4e903014a412b26928f
andwin <anders.windelhed@gmail.com>
2013-10-25 16:08:26 +0200

    Added test that generates a report

--- tests/gitflow_repo_report_spec.rb
+++ tests/gitflow_repo_report_spec.rb
@@ -33,5 +33,17 @@ describe 'The Gitflow Repo Report App', :type => :feature do
 			visit '/list/'
 			expect(page).to have_selector('.report-list li', count: 0)
 		end
+
+		it 'generates report when asked to do so' do
+			visit '/generate/'
+			expect(page).to have_content 'The report is generated!'
+
+			visit '/'
+			expect(page).to have_no_content 'No reports found!'
+
+			visit '/list/'
+			expect(page).to have_selector('.report-list li', count: 1)
+		end
+
 	end
 end
