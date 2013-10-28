91667693426f97836558f5f75a829cd56d887ac0
andwin <anders.windelhed@gmail.com>
2013-10-28 12:00:15 +0100

    Added test for comparing branch objects

--- models/branch.rb
+++ models/branch.rb
@@ -4,4 +4,15 @@ class Branch
 	attr_accessor :days_since_last_commit
 	attr_accessor :number_of_unmerged_commits
 	attr_accessor :unmerged_commits
+
+	def ==(other)
+		other.class == self.class &&
+		other.repo_name == self.repo_name &&
+		other.name == self.name
+	end
+	alias :eql? :==
+
+	def hash
+		[@name, @repo_name].hash
+    end
 end
---
+++ tests/test_branch.rb
@@ -0,0 +1,56 @@
+require 'test/unit'
+require_relative '../models/branch.rb'
+
+class TestGitOutputParser < Test::Unit::TestCase
+
+	def test_branches_are_the_same
+		branch1 = Branch.new
+		branch1.name = 'feature/TICKET-1'
+		branch1.repo_name = 'repo1'
+
+		branch2 = Branch.new
+		branch2.name = 'feature/TICKET-1'
+		branch2.repo_name = 'repo1'
+
+		assert_equal branch1, branch2
+	end
+
+	def test_branches_are_not_the_same
+		branch1 = Branch.new
+		branch1.name = 'feature/TICKET-1'
+		branch1.repo_name = 'repo1'
+
+		branch2 = Branch.new
+		branch2.name = 'feature/TICKET-1'
+		branch2.repo_name = 'repo2'
+
+		assert_not_equal branch1, branch2
+	end
+
+	def test_compare_arrays_of_branches
+		branch_a1 = Branch.new
+		branch_a1.name = 'feature/TICKET-1'
+		branch_a1.repo_name = 'repo1'
+
+		branch_a2 = Branch.new
+		branch_a2.name = 'feature/TICKET-1'
+		branch_a2.repo_name = 'repo2'
+
+		branch_b1 = Branch.new
+		branch_b1.name = 'feature/TICKET-1'
+		branch_b1.repo_name = 'repo3'
+
+		branch_b2 = Branch.new
+		branch_b2.name = 'feature/TICKET-1'
+		branch_b2.repo_name = 'repo1'
+
+		branches1 = [branch_a1, branch_a2]
+		branches2 = [branch_b1, branch_b2]
+
+		common = branches1 & branches2
+
+		assert_equal 1, common.count
+		assert_equal 'feature/TICKET-1', common[0].name
+		assert_equal 'repo1', common[0].repo_name
+	end
+end
