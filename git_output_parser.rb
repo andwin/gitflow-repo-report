a4f89eee752ed1b94a120acc3830b602f283325c
andwin <anders.windelhed@gmail.com>
2013-10-29 14:44:10 +0100

    Refactoring git_output_parser

--- git_output_parser.rb
+++ git_output_parser.rb
@@ -15,14 +15,18 @@ class GitOutputParser
 		branch.name = branch_name
 		branch.number_of_unmerged_commits = git_output.lines.count
 		branch.unmerged_commits = git_output.lines.map{ |x| x.strip }
+		branch.days_since_last_commit = parse_days_since_last_commit git_output.lines[0]
+		branch
+	end
 
-		return branch if git_output.lines[0] == nil
+	private
 
-		date_str = git_output.lines[0].split("\t")[2]
+	def self.parse_days_since_last_commit git_output_line
+
+		return if git_output_line == nil
+
+		date_str = git_output_line.split("\t")[2]
 		last_commit_date = Date.parse date_str
 		days_ago = (Date.today - last_commit_date).to_i
-		branch.days_since_last_commit = days_ago
-
-		branch
 	end
 end
