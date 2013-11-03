ed60555920aa8168dd996a58b7e2eb294ba2f320
andwin <anders.windelhed@gmail.com>
2013-11-03 16:12:50 +0100

    Displaying number of unmerged commits for unmerged branches

--- tests/test_git_output_parser.rb
+++ tests/test_git_output_parser.rb
@@ -6,10 +6,10 @@ class TestGitOutputParser < Test::Unit::TestCase
 	def test_parse_brach_info
 
 		git_output = <<-eos
-145567e	andwin	Tue Oct 8 22:36:48 2013 +0200	updated documentation
-245567e	andwin	Tue Oct 7 22:35:48 2013 +0200	critical bugfix
-345567e	andwin	Tue Oct 6 22:34:48 2013 +0200	added important feature
-445567e	andwin	Tue Oct 5 22:33:48 2013 +0200	Initial commit
+145567e andwin        Tue Oct 8 22:36:48 2013 +0200   updated documentation
+245567e andwin        Tue Oct 7 22:35:48 2013 +0200   critical bugfix
+345567e andwin        Tue Oct 6 22:34:48 2013 +0200   added important feature
+445567e andwin        Tue Oct 5 22:33:48 2013 +0200   Initial commit
 eos
 		
 		branch = GitOutputParser.parse_brach_info 'branch', 'repo', git_output
@@ -22,22 +22,7 @@ eos
 		assert_equal 4, branch.number_of_unmerged_commits
 		assert_not_nil branch.unmerged_commits
 		assert_equal 4, branch.unmerged_commits.count
-		assert_equal '145567e	andwin	Tue Oct 8 22:36:48 2013 +0200	updated documentation', branch.unmerged_commits[0]
-		assert_equal days_ago, branch.days_since_last_commit
-	end
-
-	def test_parse_merged_branch_info
-		git_output = "acb4d39	andwin	Mon Sep 16 15:42:22 2013 +0200	Final fix"
-
-		branch = GitOutputParser.parse_brach_info 'branch', 'repo', git_output
-
-
-		last_commit_date = Date.parse "Mon Sep 16 15:42:22 2013 +0200"
-		days_ago = (Date.today - last_commit_date).to_i
-
-		assert_equal 'branch', branch.name
-		assert_equal 'repo', branch.repo_name
-		assert_equal 1, branch.number_of_unmerged_commits
+		assert_equal '145567e andwin        Tue Oct 8 22:36:48 2013 +0200   updated documentation', branch.unmerged_commits[0]
 		assert_equal days_ago, branch.days_since_last_commit
 	end
 end
