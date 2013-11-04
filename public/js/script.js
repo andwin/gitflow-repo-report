b3af2bffee8282d20bd31eb471ba733a213721c6
andwin <anders.windelhed@gmail.com>
2013-11-04 16:41:40 +0100

    Code cleanup

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,35 @@
 $(function(){
-	var $window = $(window)
-	var $body = $(document.body)
+	var $window = $(window);
+	var $body = $(document.body);
 
-	var navHeight = $('.side-navbar').outerHeight(true) + 10
+	var navHeight = $('.side-navbar').outerHeight(true) + 10;
 
 	$body.scrollspy({
 		target: '.bs-sidebar',
 		offset: navHeight
-	})
+	});
 
-	$window.on('load', function () {
+	$window.on('load', function() {
 		$body.scrollspy('refresh')
-	})
+	});
 
-	$('.bs-docs-container [href=#]').click(function (e) {
+	$('.bs-docs-container [href=#]').click(function(e) {
 		e.preventDefault()
-	})
+	});
 
-	setTimeout(function () {
+	setTimeout(function() {
 		var $sideBar = $('.bs-sidebar')
 
 		$sideBar.affix({
 			offset: {
-				top: function () {
+				top: function() {
 					var offsetTop = $sideBar.offset().top
 					var sideBarMargin = parseInt($sideBar.children(0).css('margin-top'), 10)
 					var navOuterHeight = $('.bs-docs-nav').height()
 
 					return (this.top = offsetTop - navOuterHeight - sideBarMargin)
 				},
-				bottom: function () {
+				bottom: function() {
 					return (this.bottom = $('.bs-footer').outerHeight(true))
 				}
 			}
