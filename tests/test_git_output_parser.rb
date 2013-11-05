b6430cc43f6985760fd10ebcc5fe2c47e0e38332
andwin <anders.windelhed@gmail.com>
2013-11-05 14:20:55 +0100

    Fixed problem with test data

--- tests/test_git_output_parser.rb
+++ tests/test_git_output_parser.rb
@@ -6,14 +6,14 @@ class TestGitOutputParser < Test::Unit::TestCase
 	def test_parse_brach_info
 
 		git_output = <<-eos
-145567e andwin        Tue Oct 8 22:36:48 2013 +0200   updated documentation
-245567e andwin        Tue Oct 7 22:35:48 2013 +0200   critical bugfix
-345567e andwin        Tue Oct 6 22:34:48 2013 +0200   added important feature
-445567e andwin        Tue Oct 5 22:33:48 2013 +0200   Initial commit
+145567e	andwin	Tue Oct 8 22:36:48 2013 +0200	updated documentation
+245567e	andwin	Tue Oct 7 22:35:48 2013 +0200	critical bugfix
+345567e	andwin	Tue Oct 6 22:34:48 2013 +0200	added important feature
+445567e	andwin	Tue Oct 5 22:33:48 2013 +0200	Initial commit
 eos
-		
+
 		branch = GitOutputParser.parse_brach_info 'branch', 'repo', git_output
-		
+
 		last_commit_date = Date.parse git_output.lines[0]
 		days_ago = (Date.today - last_commit_date).to_i
 
@@ -22,7 +22,7 @@ eos
 		assert_equal 4, branch.number_of_unmerged_commits
 		assert_not_nil branch.unmerged_commits
 		assert_equal 4, branch.unmerged_commits.count
-		assert_equal '145567e andwin        Tue Oct 8 22:36:48 2013 +0200   updated documentation', branch.unmerged_commits[0]
+		assert_equal '145567e	andwin	Tue Oct 8 22:36:48 2013 +0200	updated documentation', branch.unmerged_commits[0]
 		assert_equal days_ago, branch.days_since_last_commit
 	end
 end
