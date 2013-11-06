70201f7e46d37183d2b1c4b364b0718b29c67171
andwin <anders.windelhed@gmail.com>
2013-11-06 18:53:50 +0100

    Changing indentation from tabs to spaces

--- public/js/script.js
+++ public/js/script.js
@@ -1,42 +1,42 @@
 $(function(){
-	var $window = $(window);
-	var $body = $(document.body);
+  var $window = $(window);
+  var $body = $(document.body);
 
-	var navHeight = $('.side-navbar').outerHeight(true) + 10;
+  var navHeight = $('.side-navbar').outerHeight(true) + 10;
 
-	$body.scrollspy({
-		target: '.bs-sidebar',
-		offset: navHeight
-	});
+  $body.scrollspy({
+    target: '.bs-sidebar',
+    offset: navHeight
+  });
 
-	$window.on('load', function() {
-		$body.scrollspy('refresh')
-	});
+  $window.on('load', function() {
+    $body.scrollspy('refresh')
+  });
 
-	$('.bs-docs-container [href=#]').click(function(e) {
-		e.preventDefault()
-	});
+  $('.bs-docs-container [href=#]').click(function(e) {
+    e.preventDefault()
+  });
 
-	setTimeout(function() {
-		var $sideBar = $('.bs-sidebar')
+  setTimeout(function() {
+    var $sideBar = $('.bs-sidebar')
 
-		$sideBar.affix({
-			offset: {
-				top: function() {
-					var offsetTop = $sideBar.offset().top
-					var sideBarMargin = parseInt($sideBar.children(0).css('margin-top'), 10)
-					var navOuterHeight = $('.bs-docs-nav').height()
+    $sideBar.affix({
+      offset: {
+        top: function() {
+          var offsetTop = $sideBar.offset().top
+          var sideBarMargin = parseInt($sideBar.children(0).css('margin-top'), 10)
+          var navOuterHeight = $('.bs-docs-nav').height()
 
-					return (this.top = offsetTop - navOuterHeight - sideBarMargin)
-				},
-				bottom: function() {
-					return (this.bottom = $('.bs-footer').outerHeight(true))
-				}
-			}
-		})
-	}, 100);
+          return (this.top = offsetTop - navOuterHeight - sideBarMargin)
+        },
+        bottom: function() {
+          return (this.bottom = $('.bs-footer').outerHeight(true))
+        }
+      }
+    })
+  }, 100);
 
-	$('.view-commits').click(function() {
-		$(this).parent().children('.modal-commits').modal('toggle');
-	});
+  $('.view-commits').click(function() {
+    $(this).parent().children('.modal-commits').modal('toggle');
+  });
 });
