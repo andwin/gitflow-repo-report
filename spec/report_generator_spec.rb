8b256f6f5093527ff950e5bc5a85d02ac7a9a579
andwin <anders.windelhed@gmail.com>
2013-11-08 22:21:53 +0100

    Code cleanup

--- spec/report_generator_spec.rb
+++ spec/report_generator_spec.rb
@@ -13,31 +13,32 @@ describe ReportGenerator do
     @test_utils.cleanup_tmp_dir
   end
 
-  context "result of call to get_repo_names" do
+  context 'result of call to get_repo_names' do
     before(:all) do
       @repos = @report_generator.get_repo_names
     end
 
-    it "should include test-repo 1 and 2" do
+    it 'should contain 2 repos' do
+      expect(@repos.count).to eq 2
+    end
+
+    it 'should include test-repo 1 and 2' do
       expect(@repos).to include 'test-repo-1'
       expect(@repos).to include 'test-repo-2'
     end
 
-    it "should contain exactly 2 repos" do
-      expect(@repos.count).to eq 2
-    end
   end
 
-  context "get master branches not merged to develop" do
+  context 'get master branches not merged to develop' do
     before(:all) do
       @branches = @report_generator.get_master_branches_not_merged_to_develop
     end
 
-    it "should contain 1 branch" do
+    it 'should contain 1 branch' do
       expect(@branches.count).to eq 1
     end
 
-    it "should be the master branch from test-repo-1" do
+    it 'should be the master branch from test-repo-1' do
       expect(@branches[0].name).to eq 'master'
       expect(@branches[0].repo_name).to eq 'test-repo-1'
     end
