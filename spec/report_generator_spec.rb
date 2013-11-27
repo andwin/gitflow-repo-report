4651eb3bf198f06573ecf0d9d1e36d434140fd04
andwin <anders.windelhed@gmail.com>
2013-11-27 22:45:35 +0100

    Ported test to RSpec

--- spec/report_generator_spec.rb
+++ spec/report_generator_spec.rb
@@ -2,8 +2,8 @@ require 'rspec'
 require_relative '../report_generator.rb'
 require_relative 'test_utils.rb'
 
-describe ReportGenerator do
 
+describe ReportGenerator do
   before(:all) do
     @test_utils = TestUtils.new
     @test_utils.setup_tmp_dir
@@ -14,53 +14,115 @@ describe ReportGenerator do
     @test_utils.cleanup_tmp_dir
   end
 
-  context 'result of call to get_repo_names' do
+  context 'release branches not merged to master' do
     before(:all) do
-      @repos = @report_generator.get_repo_names
+      @branches = @report_generator.get_release_branches_not_merged_to_master
     end
 
-    it 'should contain 2 repos' do
-      expect(@repos.count).to eq 2
+    it 'should include two branches' do
+      @branches.count.should eq 2
     end
 
-    it 'should include test-repo 1 and 2' do
-      expect(@repos).to include 'test-repo-1'
-      expect(@repos).to include 'test-repo-2'
+    it 'should include the expected branches' do
+      should_include_branch @branches, 'test-repo-1', 'release/first-release'
+      should_include_branch @branches, 'test-repo-2', 'release/first-release'
     end
   end
 
-  context 'result of call to get_master_branches_not_merged_to_develop' do
+  context 'release branches merged to master and develop' do
     before(:all) do
-      @branches = @report_generator.get_master_branches_not_merged_to_develop
+      @branches = @report_generator.get_release_branches_merged_to_master_and_develop
     end
 
-    it 'should contain 1 branch' do
-      expect(@branches.count).to eq 1
+    it 'should include 4 branches' do
+      @branches.count.should eq 4
     end
 
-    it 'should be the master branch from test-repo-1' do
-      expect(@branches[0].name).to eq 'master'
-      expect(@branches[0].repo_name).to eq 'test-repo-1'
+    it 'should include the expected branches' do
+      should_include_branch @branches, 'test-repo-1', 'release/second-release'
+      should_include_branch @branches, 'test-repo-1', 'release/third-release'
+      should_include_branch @branches, 'test-repo-2', 'release/second-release'
+      should_include_branch @branches, 'test-repo-2', 'release/third-release'
     end
   end
 
-  context 'result of call to get_release_branches_not_merged_to_develop' do
+  context 'hotfix branches not merged to develop' do
     before(:all) do
-      @branches = @report_generator.get_release_branches_not_merged_to_develop
+      @branches = @report_generator.get_hotfix_branches_not_merged_to_develop
     end
 
-    it 'should contain 2 branches' do
-      expect(@branches.count).to eq 2
+    it 'should include 2 branches' do
+      @branches.count.should eq 2
     end
 
-    it 'should include the branch release/first-release from the repo test-repo-1' do
-      expect(@branches[0].repo_name).to eq 'test-repo-1'
-      expect(@branches[0].name).to eq 'release/first-release'
+    it 'should include the expected branches' do
+      should_include_branch @branches, 'test-repo-1', 'hotfix/first-hotfix'
+      should_include_branch @branches, 'test-repo-1', 'hotfix/second-hotfix'
+    end
+  end
+
+  context 'hotfix branches not merged to master' do
+    before(:all) do
+      @branches = @report_generator.get_hotfix_branches_not_merged_to_master
     end
 
-    it 'should include the branch release/first-release from the repo test-repo-1' do
-      expect(@branches[1].repo_name).to eq 'test-repo-2'
-      expect(@branches[1].name).to eq 'release/first-release'
+    it 'should include 1 branches' do
+      @branches.count.should eq 1
     end
+
+    it 'should include the expected branches' do
+      should_include_branch @branches, 'test-repo-1', 'hotfix/first-hotfix'
+    end
+  end
+
+  context 'hotfix branches merged to master and develop' do
+    before(:all) do
+      @branches = @report_generator.get_hotfix_branches_merged_to_master_and_develop
+    end
+
+    it 'should include 1 branches' do
+      @branches.count.should eq 2
+    end
+
+    it 'should include the expected branches' do
+      should_include_branch @branches, 'test-repo-2', 'hotfix/first-hotfix'
+      should_include_branch @branches, 'test-repo-2', 'hotfix/second-hotfix'
+    end
+  end
+
+  context 'merged feature branches' do
+    before(:all) do
+      @branches = @report_generator.get_merged_feature_branches
+    end
+
+    it 'should include 1 branches' do
+      @branches.count.should eq 1
+    end
+
+    it 'should include the expected branches' do
+      should_include_branch @branches, 'test-repo-1', 'feature/TICKET-123'
+    end
+  end
+
+  context 'unmerged feature branches' do
+    before(:all) do
+      @branches = @report_generator.get_unmerged_feature_branches
+    end
+
+    it 'should include 1 branches' do
+      @branches.count.should eq 1
+    end
+
+    it 'should include the expected branches' do
+      should_include_branch @branches, 'test-repo-1', 'feature/TICKET-1337'
+    end
+  end
+
+  def should_include_branch branches, repo_name, branch_name
+    branch = Branch.new
+    branch.repo_name = repo_name
+    branch.name = branch_name
+
+    expect(branches).to include branch
   end
 end
