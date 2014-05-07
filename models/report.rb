baccec3f01fda7095b2b34701cb13397e1241780
Anders Windelhed <anders@windelhed.se>
2014-05-07 21:05:14 +0200

    GH-11: Refactoring. Broke out method to return all branches

--- models/report.rb
+++ models/report.rb
@@ -24,35 +24,29 @@ class Report
   end
 
   def total_number_of_branches
-    all_branches = master_branches_not_merged_to_develop |
-      release_branches_not_merged_to_develop |
-      release_branches_not_merged_to_master |
-      release_branches_merged_to_master_and_develop |
-      hotfix_branches_not_merged_to_develop |
-      hotfix_branches_not_merged_to_master |
-      hotfix_branches_merged_to_master_and_develop |
-      merged_feature_branches |
-      unmerged_feature_branches
-
     all_branches.count
   end
 
   def total_number_of_unmerged_commits
-    all_branches = master_branches_not_merged_to_develop |
-      release_branches_not_merged_to_develop |
-      release_branches_not_merged_to_master |
-      release_branches_merged_to_master_and_develop |
-      hotfix_branches_not_merged_to_develop |
-      hotfix_branches_not_merged_to_master |
-      hotfix_branches_merged_to_master_and_develop |
-      merged_feature_branches |
-      unmerged_feature_branches
-
     total_number_of_unmerged_commits = 0
     all_branches.each do |branch|
       total_number_of_unmerged_commits += branch.number_of_unmerged_commits
     end
 
     total_number_of_unmerged_commits
+  end
+
+  private
+  
+  def all_branches
+    master_branches_not_merged_to_develop |
+    release_branches_not_merged_to_develop |
+    release_branches_not_merged_to_master |
+    release_branches_merged_to_master_and_develop |
+    hotfix_branches_not_merged_to_develop |
+    hotfix_branches_not_merged_to_master |
+    hotfix_branches_merged_to_master_and_develop |
+    merged_feature_branches |
+    unmerged_feature_branches
   end
 end
