62556be8dd7a44d53a741e7e8a76196ba3e835c6
Anders Windelhed <anders@windelhed.se>
2014-05-07 20:59:54 +0200

    GH-11: Added method to calculate total number of unmerged commits

--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -22,4 +22,26 @@ describe Report do
 
     report.total_number_of_branches.should eq 2
   end
+
+  it 'calculates total_number_of_unmerged_commits' do
+    branch1 = Branch.new
+    branch1.repo_name = 'repo1'
+    branch1.name = 'feature/ID-123'
+    branch1.number_of_unmerged_commits = 4
+
+    branch2 = Branch.new
+    branch2.repo_name = 'repo2'
+    branch2.name = 'release/2014-04-04'
+    branch2.number_of_unmerged_commits = 3
+
+    report = Report.new
+    report.release_branches_not_merged_to_develop = []
+    report.release_branches_not_merged_to_develop.push branch1
+    report.release_branches_not_merged_to_develop.push branch2
+
+    report.release_branches_not_merged_to_master = []
+    report.release_branches_not_merged_to_master.push branch1
+
+    report.total_number_of_unmerged_commits.should eq 7
+  end
 end
